644d8c2feed9ab09fdf2be7c76f12c95758752f5
timstockford <62749234+timstockford@users.noreply.github.com>
2020-04-27 15:31:05 +0100

    initial script

--- CreateZipFile.ps1
+++ CreateZipFile.ps1
@@ -1,1 +1,12 @@
 write-host "This script will create the Zip file during build action"
+
+# Set variables
+$source = "./files"
+$destination = "./files.zip"
+
+# If zip file exists, delete file
+If(Test-path $destination) {Remove-item $destination}
+
+# Create zip file
+Compress-Archive -Path $source -DestinationPath $destination
+
