08ffa723ec1de6e3a86439a87836b04c863895c9
timstockford <62749234+timstockford@users.noreply.github.com>
2020-04-28 11:16:25 +0100

    fix error

--- triggeraction.cmd
+++ triggeraction.cmd
@@ -1,6 +1,6 @@
 git pull --tags origin master
 echo %DATE%-%TIME% >>./files/file.txt
 git add ./files/file.txt
-git tag "%DATE%-%TIME%
+git tag "%DATE%-%TIME%"
 git commit -m "Action Trigger %DATE%-%TIME%"
 git push origin master:master --tags
