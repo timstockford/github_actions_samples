b6a9fd8597a0198c1f87b0fb44863142d55a38d4
timstockford <62749234+timstockford@users.noreply.github.com>
2020-05-09 09:15:50 +0100

    updated commit msg

--- triggeraction.cmd
+++ triggeraction.cmd
@@ -3,5 +3,5 @@ git pull --tags origin master
 echo %DATE%-%TIME% >>./files/file.txt
 git add ./files/file.txt
 git tag "%TAGTEXT%"
-git commit -m "Action Trigger %DATE%-%TIME%"
+git commit -m "Action Trigger %TAGTEXT% %DATE%-%TIME%"
 git push origin master:master --tags
